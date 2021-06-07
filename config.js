1fa6713e5816546d16bec22e5be99dd0b860a434
ptiendat15 <85466435+ptiendat15@users.noreply.github.com>
2021-06-07 08:34:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,13 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnNo: 'Không coa thich :3',
+    question:'Linh ơi anh bảo Linh này, Thu kia đã hết đông giờ đã sang, Gió lạnh từng hồi miên mang, Đưa tay anh nắm bình an một đời
+ <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Oiii mừng quá 🥰. Miss Linh nhiều nhiều 😘😘',
+    messDesc: 'Tối nay mình cùng trò chuyện tiếp nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.instagram.com/ptiendat15/'
 }
